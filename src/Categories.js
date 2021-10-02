e5b3e74a84e3fc481957bd8cc6f538ec691dc0fe
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-10-01 22:34:02 -0700

    update recipe app.js

--- src/Categories.js
+++ src/Categories.js
@@ -6,21 +6,24 @@ import "./Categories.css";
 
 export default function Categories() {
     return (
+        <>
         <div className="container category-block">
             <div className="row">
                 <div className="col">
                     <img src={bites} className="category-img img-fluid" alt="bites category" />
-                    <h2 className="category-tag py-3">Chocolate Bites</h2>
+                    <h2 className="category-tag py-3">Chocolate Cup Bites</h2>
                 </div>
                 <div className="col">
                     <img src={cakes} className="category-img img-fluid" alt="cakes category" />
-                    <h2 className="category-tag py-3">Chocolate Cakes</h2>
+                    <h2 className="category-tag py-3">Chocolate Cake</h2>
                 </div>
                 <div className="col">
                     <img src={brews} className="category-img img-fluid" alt="brews category" />
-                    <h2 className="category-tag py-3">Chocolate Brews</h2>
+                    <h2 className="category-tag py-3">Hot Chocolate</h2>
                 </div>
             </div>
         </div>
+        <div className="container text-center py-2"><button className="btn btn-dark">View More Recipes</button></div>
+        </>
     );
 }
--- src/bites.js
+++ src/bites.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 
 export default function Bites() {
