caa6b290db2aedb411f20df85fcf86a684386c20
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-10-05 13:58:27 -0700

    update Navigation.js logos

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from './logo.png';
+import logoXs from './logo-xs.png'
 
 
 
@@ -7,7 +8,8 @@ export default function Navigation() {
     return (
             <div class="container-fluid px-3">
                 <a className="navbar-brand" href="/">
-                    <img src={logo} className="App-logo" alt="logo" width="350px" />
+                    <img src={logo} className="App-logo d-none d-sm-block" alt="logo" width="350px" />
+                    <img src={logoXs} className="ogo d-block d-sm-none" alt="logo" width="200px" />
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
