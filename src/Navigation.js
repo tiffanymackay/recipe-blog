1a69827d66367acca6932a09dfd8e9c728914e11
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-10-05 20:33:28 -0700

    about page styles update

--- src/Navigation.js
+++ src/Navigation.js
@@ -9,7 +9,7 @@ export default function Navigation() {
             <div class="container-fluid px-3">
                 <a className="navbar-brand" href="/">
                     <img src={logo} className="App-logo d-none d-sm-block" alt="logo" width="350px" />
-                    <img src={logoXs} className="ogo d-block d-sm-none" alt="logo" width="200px" />
+                    <img src={logoXs} className="ogo d-block d-sm-none" alt="logo-extra-small" width="200px" />
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
