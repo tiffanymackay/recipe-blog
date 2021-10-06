bd7d24496d060db0ecfca7780b97462ccfa8441e
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-10-05 19:29:31 -0700

    recipe styles update

--- src/Recipe.js
+++ src/Recipe.js
@@ -4,9 +4,9 @@ import "./Recipe.css";
 export default function Recipe({recipe}) {
     const {label, image, url, ingredients} = recipe.recipe;
     return (
-        <div className="container recipe">
+        <div className="container recipe text-center">
                         <h2>{label}</h2>
-                        <img src={image} alt={label} />
+                        <img src={image} alt={label} className="recipe-image img-fluid" />
                         <a href={url} target="_blank" rel="noopener noreferer" className="btn btn-dark py-1 my-3">Go to Recipe</a>
             </div>
         );
