7951941f2aa1cf8c6ee66762196520c353d32407
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-10-06 20:35:21 -0700

    styling css

--- src/Recipe.js
+++ src/Recipe.js
@@ -7,7 +7,7 @@ export default function Recipe({recipe}) {
         <div className="container recipe text-center">
                         <h2>{label}</h2>
                         <img src={image} alt={label} className="recipe-image img-fluid" />
-                        <a href={url} target="_blank" rel="noreferrer" className="btn py-1 my-3">Go to Recipe</a>
+                        <a href={url} target="_blank" rel="noreferrer" className="btn btn-recipe py-1 my-3">Go to Recipe</a>
             </div>
         );
 }
